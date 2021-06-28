2f194f82920be103ff590cf9c14b52881bcfa755
Arjun Sampath <arjundsampath@gmail.com>
2021-06-28 00:17:15 -0700

    added doc version updates to automation script

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@ VERSION=$1
 
 sed -i -E "/version=[^ ]/s/\".*\"/\"$VERSION\"/" setup.py
 sed -i -E "/version=[^ ]/s/\".*\"/\"$VERSION\"/" meta.yaml
+sed -i -E "/release = [^ ]/s/\".*\"/\"$VERSION\"/" docs/conf.py
 
 rm -rf dist/*
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
 
 conda build . -c bioconda
+
+pip3 install .
