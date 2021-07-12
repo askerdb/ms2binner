eba00b5cbbf98bfdbb6bd3bc6eef1d2479cec810
Arjun Sampath <arjundsampath@gmail.com>
2021-07-11 22:20:06 -0700

    Updated main to reflect visualization changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ms2binner.bin import bin_mgf
-from ms2binner.visualization import plot_ms2_components, plot_ms2_histograms
+from ms2binner.visualization import plot_ms2_components, plot_ms2_components_fine
 import argparse
 
 if __name__ == "__main__":
@@ -9,12 +9,13 @@ if __name__ == "__main__":
         parser.add_argument('-mb', '--minbin', default=50, type=float, help='minimum bin for the spectra to fall into')
         parser.add_argument('-xb', '--maxbin', default=850, type=float, help='maximum bin for the spectra to fall into')
         parser.add_argument('-p', '--maxmass', default=850, type=float, help='maximum parent mass to filter by')
-        parser.add_argument('-nc', '--components', default=10, type=float, help='number of components to split data into for plotting')
         parser.add_argument('-f', '--filename', default="binned_data.pkl", type=str, help='filepath to output data to a binary file')
-        parser.add_argument('-i', '--image', default="spectra_plot", type=str, help='filename to save visualization plot as')
         parser.add_argument('-hl', '--headless', action='store_true', help='turns on headless mode for plotting')
         parser.add_argument('-v', '--verbose', action='store_true', help='turns on verbose mode')
-        parser.add_argument('-pl', '--plot', action='store_true', help='plots and saves a visualization of the spectra')
+        parser.add_argument('-pl', '--plot', action='store_true', help='plots a visualization of the spectra')
+        parser.add_argument('-plf', '--fineplot', action='store_true', help='plots a detailed visualization of the spectra')
+        parser.add_argument('-nc', '--components', default=10, type=float, help='number of components to split data into for plotting')
+        parser.add_argument('-i', '--image', default="spectra_plot", type=str, help='filename to save visualization plot as')
         args = parser.parse_args()
 
         mgf = args.mgf
@@ -24,5 +25,7 @@ if __name__ == "__main__":
         data, bins, scans = bin_mgf(mgf_files=mgf, output_file=args.filename, min_bin=args.minbin, max_bin=args.maxbin, bin_size=args.binsize, max_parent_mass=args.maxmass, verbose=args.verbose)
 
         if args.plot:
-                plot_ms2_histograms(data, bins, num_components=args.components, output_file=args.image, headless=args.headless)
-                # plot_ms2_components(data, num_components=args.components, output_file=args.image, headless=args.headless)
+                plot_ms2_components(data, num_components=args.components, output_file=args.image, headless=args.headless)
+
+        if args.fineplot:
+                plot_ms2_components_fine(data, bins, num_components=args.components, output_file=args.image, headless=args.headless)
